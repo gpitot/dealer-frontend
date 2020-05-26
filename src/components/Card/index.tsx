ae7b413c8f704cc099b7c7d6a479a9984cd5b71b
Guillaume Pitot <gpitot@atlassian.com>
2020-05-26 16:43:08 +1000

    favicon and homepage

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,7 +9,7 @@ interface ICard {
 
 const Card = ({ isFlippable, card }: ICard) => {
   const [isOpen, setOpen] = useState<boolean>(true);
-  const src = isOpen ? BASE_URL + card + ".png" : BASE_URL + "red_back.png";
+  const src = isOpen ? BASE_URL + card + ".jpg" : BASE_URL + "red_back.jpg";
 
   const handleClick = () => {
     if (isFlippable) {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./style.scss";
 const Home = () => (
-  <>
+  <div className="home">
     <h1>Automatic Dealer</h1>
     <Link to="/room">Join room</Link>
-  </>
+  </div>
 );
 
 export default Home;
